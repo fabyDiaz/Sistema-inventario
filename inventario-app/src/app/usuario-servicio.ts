c46783fa0daaf6991d3a363f4fa42d2e84bcacc9
fabyDiaz <fbl.diaz.n@gmail.com>
2025-06-21 20:37:40 -0400

    se agrega inicio de sesión con autenticación

--- inventario-app/src/app/usuario-servicio.ts
+++ inventario-app/src/app/usuario-servicio.ts
@@ -8,12 +8,45 @@ import { Observable } from 'rxjs';
 })
 export class UsuarioServicio {
 
-  private urlBase = "http://localhost:8080/inventario-app/usuarios";
+  private urlBase = "http://localhost:8080/inventario-app/auth";
   private clienteHttp = inject(HttpClient);
   
   constructor() { }
 
   agregarUsuario(usuario: Usuario):Observable<Object>{
-      return this.clienteHttp.post(this.urlBase, usuario);
+      //return this.clienteHttp.post(this.urlBase, usuario);
+      return this.clienteHttp.post(`${this.urlBase}/nuevo-usuario`, usuario);
   }
+
+  login(usuario: string, contrasena: string): Observable<any> {
+    /*return this.clienteHttp.post<{ token: string }>(`${this.urlBase}/login`, {
+      usuario,
+      contrasena
+    });*/
+
+    const body = { usuario, contrasena };
+    return this.clienteHttp.post<any>(`${this.urlBase}/login`, body, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  }
+
+  guardarToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+
+  obtenerToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  cerrarSesion(): void {
+    localStorage.removeItem('token');
+  }
+
+  estaAutenticado(): boolean {
+    return !!this.obtenerToken();
+  }
+
+
 }
